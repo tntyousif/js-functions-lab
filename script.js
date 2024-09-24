27140f90418e0ef4effe6e6f9797aa1ee3deb05e
tntyousif <yousif02bh@gmail.com>
2024-09-24 16:55:26 +0300

    ex 9

--- script.js
+++ script.js
@@ -181,7 +181,25 @@ console.log('Exercise 8 Result:', convertTemperature(32, "C"));
 
 
 //ex9
+/*
+Exercise 9: basicCalculator()
 
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
 
 
 //ex10
