bd68461f9a331e4568ae23b84bbbbd4529a84118
tntyousif <yousif02bh@gmail.com>
2024-09-25 14:30:49 +0300

    all done

--- script.js
+++ script.js
@@ -287,3 +287,19 @@ console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
 
 
 //ex 12
+
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+function numArgs() {
+    return arguments.length;
+}
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
